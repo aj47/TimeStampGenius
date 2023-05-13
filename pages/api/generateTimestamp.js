2b7b92c46186ce095bf6a120603dc7a412f7fb2e
AJ <arash@appricot.io>
2023-05-13 15:07:31 +0700

    dynamodb credit check

--- pages/api/generateTimestamp.js
+++ pages/api/generateTimestamp.js
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  UpdateItemCommand,
+} from "@aws-sdk/client-dynamodb";
+const client = new DynamoDBClient({});
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
@@ -9,15 +15,47 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
+  //Check if logged in
   const session = await getServerSession(req, res, authOptions);
   if (!session) {
     res.status(401);
-  } else {
-    const completion = await openai.createCompletion({
-      max_tokens: 3040,
-      model: "text-davinci-003",
-      prompt: `take the following transcript spoken by @techfren during a livestream and write a few words for a SHORT single line timestamp description: '${req.body.currentTextChunk}' `,
-    });
-    res.status(200).json({ completionText: completion.data.choices[0].text });
+    return;
   }
+
+  //Check if has credit
+  const { Item } = await client.send(
+    new GetItemCommand({
+      TableName: process.env.USER_TABLE,
+      Key: {
+        email: { S: session.user.email },
+      },
+    })
+  );
+  if (Item.credit <= 0) {
+    res.status(200).json({ error: "Insufficient credit" });
+    return;
+  }
+
+  //Perform LLM call
+  const completion = await openai.createCompletion({
+    max_tokens: 3040,
+    model: "text-davinci-003",
+    prompt: `take the following transcript spoken during a livestream and write a few words for a SHORT single line timestamp description: '${req.body.currentTextChunk}' `,
+  });
+
+  //Decrease credit
+  await client.send(
+    new UpdateItemCommand({
+      TableName: process.env.USER_TABLE,
+      Key: {
+        email: { S: session.user.email },
+      },
+      UpdateExpression: "SET credit = credit - :val",
+      ExpressionAttributeValues: {
+        ":val": { N: '1' },
+      },
+      ReturnValues: "UPDATED_NEW",
+    })
+  );
+  res.status(200).json({ completionText: completion.data.choices[0].text });
 }
