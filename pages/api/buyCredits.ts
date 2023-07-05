de4ae0eab4ae7e2f468221918d2f433556347cef
Arash Joobandi <arash@appricot.io>
2023-07-05 15:54:01 +1000

    fix 3000 credit package

--- pages/api/buyCredits.ts
+++ pages/api/buyCredits.ts
@@ -12,7 +12,7 @@ const stripe = Stripe(
 const storeItems = new Map([
   [1, { priceInCents: 500, name: "500 credits", credits: 500 }],
   [2, { priceInCents: 900, name: "1000 credits", credits: 1000 }],
-  [3, { priceInCents: 2000, name: "3000 credits", credits: 30000 }],
+  [3, { priceInCents: 2000, name: "3000 credits", credits: 3000 }],
 ]);
 
 export default async function handler(
