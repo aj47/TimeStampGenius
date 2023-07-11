688f889763186af23690ad4bbff1db59864af8c6
Arash Joobandi <arash@appricot.io>
2023-07-12 09:37:50 +1000

    enable cors on transcription

--- pages/api/getTranscription.js
+++ pages/api/getTranscription.js
@@ -3,6 +3,18 @@ const { YoutubeTranscript } = require("youtube-transcript");
 const logger = require("pino")();
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
   await YoutubeTranscript.fetchTranscript(req.body.id).then((response) => {
     logger.info({
       youtubeID: req.body.id,
