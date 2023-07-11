5acdb156b42d011b6e2b426a237f5b0ed5224efd
Arash Joobandi <arash@appricot.io>
2023-07-12 09:44:33 +1000

    cors attempt 2

--- pages/api/getTranscription.js
+++ pages/api/getTranscription.js
@@ -2,19 +2,24 @@
 const { YoutubeTranscript } = require("youtube-transcript");
 const logger = require("pino")();
 
-export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+async function handler(req, res) {
   await YoutubeTranscript.fetchTranscript(req.body.id).then((response) => {
     logger.info({
       youtubeID: req.body.id,
@@ -23,3 +28,5 @@ export default async function handler(req, res) {
     res.status(200).json({ transcript: response });
   });
 }
+
+module.exports = allowCors(handler)
