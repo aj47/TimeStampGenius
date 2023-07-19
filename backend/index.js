dd73ecc753b66b8f9a2d725524ed7b2177e8e156
Arash Joobandi <yspdev@gmail.com>
2023-07-19 13:42:55 +1000

    cleanup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,37 @@
+const { DynamoDBClient, PutItemCommand } = require("@aws-sdk/client-dynamodb");
+const client = new DynamoDBClient({});
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+  try {
+    console.log(event, "event");
+    const clientIP = event.requestContext.identity.sourceIp;
+    console.log(clientIP, "clientIP");
+    // what happens when we try add the same PK twice?
+    const command = new PutItemCommand({
+      TableName: process.env.DB,
+      // For more information about data types,
+      // see https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.DataTypes and
+      // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Programming.LowLevelAPI.html#Programming.LowLevelAPI.DataTypeDescriptors
+      Item: {
+        PK: { S: clientIP },
+        SK: { S: (Math.floor(new Date().getTime() / 1000) + 360).toString() },
+        ttl: { N: (Math.floor(new Date().getTime() / 1000) + 360).toString() },
       },
-      null,
-      2
-    ),
-  };
+    });
+    const response = await client.send(command);
+    console.log(response, "response");
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Go Serverless v3.0! Your function executed successfully!",
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  } catch (e) {
+    console.error(e);
+  }
 };
