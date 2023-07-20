40bbb8024a4712537882543fd71e880b92af895d
Arash Joobandi <yspdev@gmail.com>
2023-07-20 20:16:59 +1000

    reduce token length

--- pages/api/generateTimestamp.js
+++ pages/api/generateTimestamp.js
@@ -38,11 +38,16 @@ export default async function handler(req, res) {
   }
 
   //Perform LLM call
-  const completion = await openai.createCompletion({
-    max_tokens: 3040,
-    model: "text-davinci-003",
-    prompt: `write a SHORT (less than 5 words) SINGLE LINE in description, mentioning keywords based on the following spoken transcript: '${req.body.currentTextChunk}'`,
-  });
+  let completion = null;
+  try {
+    completion = await openai.createCompletion({
+      max_tokens: 3040,
+      model: "text-davinci-003",
+      prompt: `write a SHORT (less than 5 words) SINGLE LINE in description, mentioning keywords based on the following spoken transcript: '${req.body.currentTextChunk}'`,
+    });
+  } catch (e) {
+    console.log(JSON.stringify(e.response.data.error), "e");
+  }
 
   //Decrease credit
   await client.send(
